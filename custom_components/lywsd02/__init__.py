cee176754922b84f4972da4fa2ce930428ec27ae
Lars Randers <lars@nstec.dk>
2024-03-05 16:04:34 +0100

    Add C/F and 12/24 toggles

--- custom_components/lywsd02/__init__.py
+++ custom_components/lywsd02/__init__.py
@@ -17,6 +17,7 @@ DOMAIN = "lywsd02"
 _LOGGER = logging.getLogger(__name__)
 
 _UUID_TIME = 'EBE0CCB7-7A0A-4B0C-8A1A-6FF2997DA3A6'
+_UUID_TEMO = 'EBE0CCBE-7A0A-4B0C-8A1A-6FF2997DA3A6'
 
 def get_localized_timestamp():
     now = int(time.time())
@@ -56,8 +57,30 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
         _LOGGER.info(f"Found '{ble_device}' - Attempting to update time.")
 
+        temo_set = False
+        ckmo_set = False
+        temo = call.data.get('temp_mode', '') or "x"
+        temo = temo.upper()
+        _LOGGER.debug(f"temo var: {temo}")
+
+        if temo in 'CF':
+            data_temp_mode = struct.pack('B', (0x01 if temo == 'F' else 0xFF))
+            _LOGGER.debug(f"Will set temp_mode")
+            temo_set = True
+
+        ckmo = call.data.get('clock_mode', 0)
+        _LOGGER.debug(f"ckmo var: {ckmo}")
+        if ckmo in [12, 24]:
+            data_clock_mode = struct.pack('IHB', 0, 0, 0xaa if ckmo == 12 else 0x00)
+            _LOGGER.debug(f"Will set clock_mode")
+            ckmo_set = True
+
         async with BleakClient(ble_device) as client:
             await client.write_gatt_char(_UUID_TIME, data)
+            if temo_set:
+                await client.write_gatt_char(_UUID_TEMO, data_temp_mode)
+            if ckmo_set:
+                await client.write_gatt_char(_UUID_TIME, data_clock_mode)
 
         _LOGGER.info(f"Done - refreshed time on '{mac}' to '{timestamp}' with offset of '{tz_offset}' hours.")
 
